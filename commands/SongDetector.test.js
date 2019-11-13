fcff24c8e1d3c9e0910843e224d1399703875150
David Whitney <david@davidwhitney.co.uk>
2019-11-13 17:00:15 +0000

    Fixed test.

--- commands/SongDetector.test.js
+++ commands/SongDetector.test.js
@@ -1,14 +1,14 @@
 const SongDetector = require("./SongDetector");
 const fs = require("fs");
 
-describe("Something", () => {
+describe("Song detector", () => {
 
-    it("Works",  async () => {
+    it("Can detect song that we know about",  async () => {
         const sut = new SongDetector();
         const songContents = await fs.readFileSync("./test-data/02 - Jingle Bell Rock.mp3");
 
         const result = await sut.execute(songContents);
 
-        expect(result).toBe("blah");
+        expect(result.body.toLowerCase()).toBe("jingle bell rock");
     });
 });
