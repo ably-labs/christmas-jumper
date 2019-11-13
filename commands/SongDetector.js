fea02d26e0f5735b401d9f56aa8d91d94697d467
David Whitney <david@davidwhitney.co.uk>
2019-11-13 16:58:25 +0000

    Looks like it doesn't suck

--- commands/SongDetector.js
+++ commands/SongDetector.js
@@ -1,11 +1,6 @@
-const  {StorageSharedKeyCredential} = require("@azure/storage-blob");
-
-const { TokenCredential } = require( "azure-storage");
-
-var axios = require("axios");
-var request = require("request");
+const { StorageSharedKeyCredential } = require("@azure/storage-blob");
 const { BlobServiceClient } = require("@azure/storage-blob");
-const { DefaultAzureCredential } = require("@azure/identity");
+const axios = require("axios");
 const uuidv1 = require('uuid/v1');
 
 class SongDetector {
@@ -13,29 +8,29 @@ class SongDetector {
     };
 
     async execute(bytes) {
+        const url = await this.uploadToBlobStorage(bytes);
+        const result = await this.pushToRecognitionApi(url);
+        return this.ok(result.data.result.title);
+    };
 
-        const bufferValue = Buffer.from(bytes);
-        let base64data = bufferValue.toString('base64');
-
+    async uploadToBlobStorage(bytes) {
         const account = "jumperstorage";
         const key = "I9rBXWcRZYWBXzBNFoY0tEkAPAF46a9W5jIMoCrVTtThdISCq00iM28kMb3Ni2Zq2/CjbH+YhrOdKouwLGcp/A==";
         const defaultAzureCredential = new StorageSharedKeyCredential(account, key);
-
         const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net`, defaultAzureCredential);
 
         const unique = uuidv1();
         const containerClient = blobServiceClient.getContainerClient("jumper");
         const blockBlobClient = containerClient.getBlockBlobClient(unique);
         const uploadBlobResponse = await blockBlobClient.upload(bytes, bytes.length);
-        console.log(uploadBlobResponse);
+        return `https://jumperstorage.blob.core.windows.net/jumper/${unique}`;
+    }
 
-        const url = `https://jumperstorage.blob.core.windows.net/jumper/${unique}`;
+    async pushToRecognitionApi(url) {
         const token = "e559e284cffb189e3b3ab07f0446e2b9";
-
         const result = await axios.post(`https://api.audd.io/?api_token=${token}&return=timecode&url=${url}`);
-
-        return this.ok(result);
-    };
+        return result;
+    }
 
     ok(value) { return { status: 200, body: value } };
 }
