a777e63ad39d70a9316c19d61774eed6ba62a4ac
David Whitney <david@davidwhitney.co.uk>
2019-11-14 22:58:25 +0000

    Split app from server for full e2e tests.

---
+++ app.js
@@ -0,0 +1,23 @@
+const JumperApi = require("./commands/JumperApi");
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.static("public"));
+app.use("/", express.static(__dirname + "/public", { index: "index.html" }));
+
+const jumperApiSingleton = new JumperApi();
+
+app.get("/active-image", async (request, response) => {
+    const result = await jumperApiSingleton.getActiveImageKey();
+    response.send(result.body);
+});
+
+app.post("/what-song", async (request, response) => {
+    const result = await jumperApiSingleton.detectSongFromClip(request.body.bytes);
+    response.send(result.body);
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,4 @@
-const JumperApi = require("./commands/JumperApi");
-const express = require("express");
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(express.static("public"));
-app.use("/", express.static(__dirname + "/public", { index: "index.html" }));
-
-const jumperApiSingleton = new JumperApi();
-
-app.get("/active-image", async (request, response) => {
-  const result = await jumperApiSingleton.getActiveImageKey();
-  response.send(result.body);
-});
-
-app.post("/what-song", async (request, response) => {
-  const result = await jumperApiSingleton.detectSongFromClip(request.body.bytes);
-  response.send(result.body);
-});
+const app = require("./app");
 
 const port = process.env.PORT | 12271;
 const listener = app.listen(port, function() {
