637ce2db46699d358fe292779d4057839c774f52
David Whitney <david@davidwhitney.co.uk>
2019-11-20 23:07:25 +0000

    Tidy up and reorg.

--- hardware/JumperFirmware/JumperFirmware.ino
+++ hardware/JumperFirmware/JumperFirmware.ino
@@ -1,6 +1,5 @@
 #include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
-
 #include <ESP8266WiFi.h>
 #include <cstring>
 #ifdef __AVR__
@@ -9,33 +8,48 @@
 
 #define PIN 4
 #define NUM_LIGHTS  256
+constexpr auto ssid = "asgard_router1";;
+constexpr auto password = "godhatesfangs";
+constexpr auto default_delay = 1000;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS, PIN, NEO_GRB + NEO_KHZ800);
-typedef struct { String key; int value[256][3]; } key_value_pair;
 
-auto ensure_wifi_connected(const char* ssid, const char* password) -> void
+typedef struct { String key; int value[256][3]; } key_value_pair;
+typedef struct { String image_key; int frame_index; int frame_duration; } image_identity;
+
+auto ensure_wifi_connected() -> void
 {
 	if (WiFi.status() == WL_CONNECTED)
 	{
-		Serial.println("WiFi already connected.");
+		Serial.println(F("WiFi already connected."));
 		return;
 	}
 	
-	Serial.print("Connecting to ");
+	Serial.print(F("Connecting to "));
 	Serial.print(ssid);
 	Serial.println();
 
 	WiFi.begin(ssid, password);
 
 	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
+		delay(100);
 		Serial.print(".");
 	}
-	Serial.println("");
-	Serial.println("WiFi connected");
+	
+	Serial.println(F("WiFi connected"));
 }
 
-auto http_get(String urlToReq) -> String
+auto setup() -> void
+{
+	strip.begin();
+	strip.show();
+	Serial.begin(115200);
+	delay(100);
+
+	ensure_wifi_connected();
+}
+
+auto http_get(const String urlToReq) -> String
 {
 	Serial.println("Requesting: " + urlToReq);
 	
@@ -50,27 +64,38 @@ auto http_get(String urlToReq) -> String
 	WiFiClient client;
 
 	if (!client.connect(host, port)) {
-		Serial.println("connection failed");
+		Serial.println(F("connection failed."));
 		return "";
 	}
 
-	// This will send the request to the server
-	client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
-	delay(500);
-
-	String response;
-	while (client.available()) { response = client.readString(); }
-
-	Serial.println("Request read response.");
+	client.println("GET " + url + " HTTP/1.0");
+	client.println("Host: " + host);
+	client.println(F("Connection: close"));
+	if (client.println() == 0) {
+		Serial.println(F("Failed to send request"));
+		return "";
+	}
 	
-	const String delimiter = "\r\n\r\n";
+	char status[32] = { 0 };
+	client.readBytesUntil('\r', status, sizeof(status));
+	if (strcmp(status + 9, "200 OK") != 0) {
+		Serial.println(F("Status code wasn't 200."));
+		return "";
+	}
+		
+	String response;
+	while (client.available())
+	{
+		response += client.readStringUntil('\r');
+	}
+	
+	const String delimiter = F("Connection: close");
 	const auto index_of_close = response.lastIndexOf(delimiter);
 	auto body = response.substring(index_of_close + delimiter.length());
 	body.trim();
 
-	Serial.println("Request completed, returned:");
+	Serial.println(F("Request completed, returned:"));
 	Serial.println(body);
-
 	return body;
 }
 
@@ -82,50 +107,32 @@ auto error_lights() -> void
 
 auto update_lights(const DynamicJsonDocument& doc) -> void
 {
-	Serial.println("Updating lights");
-
 	for (auto i = 0; i < NUM_LIGHTS; i++) {
-		auto image_frame = doc["frame"]["b"];
+		auto image_frame = doc[F("frame")][F("b")];
 		const auto palette_ref = image_frame[i].as<int>();
-		const auto hex_code = doc["palette"][palette_ref].as<const char*>();
+		const auto hex_code = doc[F("palette")][palette_ref].as<const char*>();
 				
 		int r, g, b;
 		sscanf(hex_code, "%02x%02x%02x", &r, &g, &b);
+		
 		strip.setPixelColor(i, strip.Color(r, g, b));
 	}
 
-	Serial.println("Colours set");
-}
-
-const auto ssid = "asgard_router1";
-const auto password = "godhatesfangs";
-
-auto setup() -> void
-{
-	strip.begin();
 	strip.show();
-	Serial.begin(115200);
-	delay(100);
 	
-	ensure_wifi_connected(ssid, password);
+	Serial.println(F("Lights set"));
 }
 
-typedef struct
-{
-	String image_key;
-	int frame_index;
-	int frame_duration;
-} image_identity;
-
-const int default_delay = 1000;
 image_identity current_image = { "_", -1, default_delay };
 
 auto loop() -> void
 {
-	ensure_wifi_connected(ssid, password);
+	Serial.println(F("Loop()"));
+	ensure_wifi_connected();
 
 	auto api_path = "http://192.168.1.75:12271/active-image-frames?currentImageKey=";
 	auto url_to_req = api_path + current_image.image_key + "&currentFrameIndex=" + current_image.frame_index;
+	
 	const auto json = http_get(url_to_req);
 	if (json.equals("")) {
 		delay(default_delay);
@@ -135,14 +142,15 @@ auto loop() -> void
 	DynamicJsonDocument doc(20000);
 	const DeserializationError error = deserializeJson(doc, json);
 	if (error) {
+		Serial.println(error.c_str());
 		delay(default_delay);
 		return;
 	}
 
-	const auto total_frames = doc["frameCount"].as<int>();
-	const auto image_key = doc["imageKey"].as<String>();
-	const auto frame_index = doc["frameIndex"].as<int>();
-	const auto frame_display_duration = doc["frame"]["duration"].as<int>();
+	const auto total_frames = doc[F("frameCount")].as<int>();
+	const auto image_key = doc[F("imageKey")].as<String>();
+	const auto frame_index = doc[F("frameIndex")].as<int>();
+	const auto frame_display_duration = doc[F("frame")][F("duration")].as<int>();
 
 	current_image.image_key = image_key;
 	current_image.frame_index = frame_index;
@@ -151,8 +159,7 @@ auto loop() -> void
 	Serial.println("Displaying " + image_key + " at frame " + current_image.frame_index + " wth a delay of " + current_image.frame_duration);
 	
 	update_lights(doc);
-	strip.show();
 
 	delay(current_image.frame_duration);
-	Serial.println("Looping...");
+	Serial.println(F("Finished waiting."));
 }
