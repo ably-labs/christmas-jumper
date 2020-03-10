c030037462e6d90ef3bec4e09df4b23f061c5743
David Whitney <david@davidwhitney.co.uk>
2020-03-10 15:00:36 +0000

    Added mqtt test receiver.

---
+++ mqtt-receiver.js
@@ -0,0 +1,36 @@
+// This is for dev-testing only.
+const mqtt = require('mqtt')
+const express = require("express");
+const app = express();
+const config = require("./config");
+
+const creds = config["ably-api-key"].split(':');
+
+var client  = mqtt.connect("mqtts://mqtt.ably.io", {
+    username: creds[0],
+    password: creds[1]
+});
+
+client.on('connect', function () {
+  client.subscribe('jumper', function (err) {
+    if (!err) {
+      client.publish('jumper', 'Hello mqtt')
+    }
+  })
+});
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(topic)
+  console.log(message.toString())
+  client.end()
+});
+
+app.get("/status", (req, res) => {
+    res.write({status: "ok"});
+});
+
+const port = process.env.PORT || 12270;
+const listener = app.listen(port, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
