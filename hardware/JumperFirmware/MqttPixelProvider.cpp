c534337af6fb1339ba6aba7678edc323110d4bea
David Whitney <david@davidwhitney.co.uk>
2020-03-13 15:40:45 +0000

    Simpler MQTT client annoyingly requires a manual patch to a dependency first (see comment in MQTT provider)

--- hardware/JumperFirmware/MqttPixelProvider.cpp
+++ hardware/JumperFirmware/MqttPixelProvider.cpp
@@ -3,6 +3,15 @@
 #include "ApiResponseParser.h"
 #include <WiFiClientSecure.h>
 
+// This doesn't work in the Arduino IDE
+// You have to override the value defined in PubSubClient.h
+// But once you do, this implementation is fine.
+// Gonna try another libary to avoid this.
+
+#define MQTT_MAX_PACKET_SIZE 4096
+
+#include <PubSubClient.h>
+
 mqttConfiguration mqttConf = {
   "mqtt.ably.io",
   8883,
@@ -11,16 +20,10 @@ mqttConfiguration mqttConf = {
   "3b b1 e6 46 12 f9 f4 ac 53 59 f4 97 99 ee 35 c9 3b 3b 46 11"
 };
 
-boolean setupCalled = false;
 WiFiClientSecure espClient;
+PubSubClient client(espClient);
 
 api_response lastMessage;
-
-#if defined(ESP32)
-#include <ssl_client.h>
-#include <PubSubClient.h>
-
-PubSubClient client(espClient);
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String framedata = "";
@@ -38,19 +41,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
   lastMessage = api_response_parser::parse(framedata);
 }
 
-void setupMqtt() 
-{
-}
-
-void checkForMessages()
-{  
-  client.loop();
-}
-
 void ensureConnected() 
 {
   if (!client.connected())
-  {
+  {    
+    espClient.setFingerprint(mqttConf.certificate);
     client.setServer(mqttConf.server, mqttConf.port);
     client.setCallback(callback);
 
@@ -74,82 +69,14 @@ void ensureConnected()
   }
 }
 
-#else
-
-// ESP8266 version.
-
-#include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
-
-Adafruit_MQTT_Client mqtt(&espClient, mqttConf.server, mqttConf.port, mqttConf.user, mqttConf.user, mqttConf.password);
-Adafruit_MQTT_Subscribe jumperfeed = Adafruit_MQTT_Subscribe(&mqtt, "jumper");
-
-void callback(char* data, uint16_t len) 
-{
-  Serial.println("esp8266 callback");
-  String framedata = String(data);
-  Serial.println(framedata);
-  
-  if (framedata.equals("")) {
-    return;
-  }
-  
-  lastMessage = api_response_parser::parse(framedata);
-}
-
-void setupMqtt() 
-{
-  espClient.setFingerprint(mqttConf.certificate);
-  jumperfeed.setCallback(callback);  
-  mqtt.subscribe(&jumperfeed);
-}
-
-void ensureConnected() 
-{
-  if (mqtt.connected()) {
-    return;
-  }
-
-  Serial.print("Connecting to MQTT... ");
-
-  int8_t ret;
-  while ((ret = mqtt.connect()) != 0) {
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);
-  }
-  
-  Serial.println("MQTT Connected!");
-}
-
-void checkForMessages()
-{    
-  mqtt.processPackets(10000);
-  
-  if(! mqtt.ping()) {
-    mqtt.disconnect();
-  }
-}
-
-#endif
-
-
 mqtt_pixel_provider::mqtt_pixel_provider() {
   lastMessage = no_data_loaded();
 }
 
 api_response mqtt_pixel_provider::get_image_data(image_identity* current_image_ptr)
-{  
-  if(!setupCalled)
-  {
-    setupMqtt();
-    setupCalled = true;
-  }
-  
+{    
   ensureConnected();
-  checkForMessages();
+  client.loop();
   
   const auto returnThis = lastMessage;
   lastMessage = no_data_loaded();
